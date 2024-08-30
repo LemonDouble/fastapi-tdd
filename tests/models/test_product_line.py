2db0d17efbc0f962ae3d810311b42145f6d3beb5
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2024-08-30 16:07:59 +0900

    test : product line 테스트 추가

---
+++ tests/models/test_product_line.py
@@ -0,0 +1,94 @@
+from sqlalchemy import Boolean, DateTime, Enum, Float, Integer, Numeric, String, Text, UUID
+
+
+def test_model_structure_table_exists(db_inspector):
+    """
+    테이블이 존재하는지 확인
+    """
+    assert db_inspector.has_table("product_line")
+
+
+def test_model_structure_column_data_types(db_inspector):
+    """
+    테이블에 있는 데이터 타입과 필드들이 정상적으로 존재하는지 확인
+    """
+    table = "product_line"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["price"]["type"], type(Numeric(precision=5, scale=2)))
+    assert isinstance(columns["sku"]["type"], UUID)
+    assert isinstance(columns["stock_qty"]["type"], Integer)
+    assert isinstance(columns["is_active"]["type"], Boolean)
+    assert isinstance(columns["order"]["type"], Integer)
+    assert isinstance(columns["weight"]["type"], Float)
+    assert isinstance(columns["created_at"]["type"], DateTime)
+    assert isinstance(columns["product_id"]["type"], Integer)
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    """
+    테이블의 Nullable, Non-Nullable 필드가 정상 설정되어 있는지 테스트
+    """
+    table = "product_line"
+    columns = db_inspector.get_columns(table)
+
+    expected_nullable = {"id":         False,
+                         "price":      False,
+                         "sku":        False,
+                         "stock_qty":  False,
+                         "is_active":  False,
+                         "order":      False,
+                         "weight":     False,
+                         "created_at": False,
+                         "product_id": False,
+                         }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(column_name), f"column {column_name} is not as null expected"
+
+
+def test_model_structure_column_constraints(db_inspector):
+    """
+    테이블의 Constraints 설정이 정상 설정되어 있는지 테스트
+    """
+    table = "product_line"
+    constraints = db_inspector.get_check_constraints(table)
+
+    assert any(constraint["name"] == "product_line_order_range" for constraint in constraints)
+    assert any(constraint["name"] == "product_line_check_price" for constraint in constraints)
+
+
+def test_model_structure_default_values(db_inspector):
+    """
+    테이블의 Default Value가 정상 설정되어 있는지 테스트
+    """
+    table = "product_line"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert columns["stock_qty"]["default"] == "0"
+    assert columns["is_active"]["default"] == "false"
+
+
+def test_model_structure_unique_constraints(db_inspector):
+    """
+    테이블의 Unique Constrints가 잘 적용되어 있는지 확인
+    """
+    table = "product_line"
+    constraints = db_inspector.get_unique_constraints(table)
+
+    assert any(constraint["name"] == "uq_product_line_sku" for constraint in constraints)
+    assert any(constraint["name"] == "uq_product_line_order_product_id" for constraint in constraints)
+
+
+def test_model_structure_foreign_key(db_inspector):
+    """
+    테이블의 FK가 잘 설정되어 있는지 확인
+    """
+    table = "product_line"
+    foreign_keys = db_inspector.get_foreign_keys(table)
+
+    product_foreign_key = next((fk for fk in foreign_keys if fk["constrained_columns"] == ["product_id"]), None)
+
+    assert product_foreign_key is not None
