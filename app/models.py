bfa25ac8f27154b85e3d14d63437b03680129078
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2024-08-30 15:16:55 +0900

    feat : product 테이블 구현 추가

--- app/models.py
+++ app/models.py
@@ -1,21 +1,54 @@
-from sqlalchemy import Boolean, CheckConstraint, Column, Integer, String, UniqueConstraint
+import sqlalchemy
+from sqlalchemy import Boolean, CheckConstraint, Column, DateTime, Enum, ForeignKey, Integer, String, Text, \
+    UUID, UniqueConstraint, text
 
 from app.db_connection import Base
 
 
 class Category(Base):
-    __tablename__ = 'category'
+    __tablename__ = "category"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     slug = Column(String(120), nullable=False)
     is_active = Column(Boolean, nullable=False, default=False, server_default="false")
     level = Column(Integer, nullable=False, default=100, server_default="100")
-    parent_id = Column(Integer, nullable=True)
+    parent_id = Column(Integer, ForeignKey("category.id"), nullable=True, )
 
     __table_args__ = (
-        CheckConstraint("LENGTH(name) > 0", name="name_length_check"),
-        CheckConstraint("LENGTH(slug) > 0", name="slug_length_check"),
+        CheckConstraint("LENGTH(name) > 0", name="category_name_length_check"),
+        CheckConstraint("LENGTH(slug) > 0", name="category_slug_length_check"),
         UniqueConstraint("name", "level", name="uq_category_name_level"),
         UniqueConstraint("slug", name="uq_category_slug"),
     )
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True)
+    pid = Column(UUID(as_uuid=True), unique=True, nullable=False, server_default=text("uuid_generate_v4()"))
+    name = Column(String(200), nullable=False)
+    slug = Column(String(220), nullable=False)
+    description = Column(Text, nullable=True)
+    is_digital = Column(Boolean, nullable=False, default=False, server_default="false")
+    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"), )
+    updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"),
+                        onupdate=sqlalchemy.func.now())
+    is_active = Column(Boolean, nullable=False, default=False, server_default="false")
+    stock_status = Column(Enum("oos", "is", "obo", name="status_enum"), nullable=False, server_default="oos")
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False, )
+    seasonal_id = Column(Integer, ForeignKey("seasonal_event.id"), nullable=True, )
+
+    __table_args__ = (
+        CheckConstraint("LENGTH(name) > 0", name="product_name_length_check"),
+        CheckConstraint("LENGTH(slug) > 0", name="product_slug_length_check"),
+        UniqueConstraint("name", name="uq_product_name"),
+        UniqueConstraint("slug", name="uq_product_slug"),
+    )
+
+
+class SeasonalEvent(Base):
+    __tablename__ = "seasonal_event"
+
+    id = Column(Integer, primary_key=True)
