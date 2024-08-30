227f11a3dd9d5a9508d126a52c8936a99eb5d452
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2024-08-30 14:15:06 +0900

    test : 테스트 이름 기반 자동 marker 생성 기능 추가

---
+++ tests/utils/pytest_utils.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    """
+    pytest 테스트 이름을 읽어
+    model이 있으면 model 마커를,
+    structure가 있으면 structure 마커를 추가해 줌
+    """
+    for item in items:
+        if "model" in item.name:
+            item.add_marker(pytest.mark.model)
+        if "structure" in item.name:
+            item.add_marker(pytest.mark.model_structure)
