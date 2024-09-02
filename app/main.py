aa9874c714c960d72af260daf102b2be9a240902
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-02 17:59:04 +0900

    feat : 표준 라이브러리 이용해 Logging 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import logging.config
+
+logging.config.fileConfig("logging.config", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-
-@app.get("/hello")
-def hello():
-    return {"Hello": "How you doing?"}
+@app.get("/")
+def test():
+    try:
+        result = 1 / 0
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
