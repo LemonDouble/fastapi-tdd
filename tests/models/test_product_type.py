e24ed39e45b69d2cdadf145ae06bdd33745c23d2
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-02 15:53:07 +0900

    test : product_type 테스트 추가

---
+++ tests/models/test_product_type.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Boolean, Integer, String
+
+
+def test_model_structure_table_exists(db_inspector):
+    """
+    테이블이 존재하는지 확인
+    """
+    assert db_inspector.has_table("product_type")
+
+
+def test_model_structure_column_data_types(db_inspector):
+    """
+    테이블에 있는 데이터 타입과 필드들이 정상적으로 존재하는지 확인
+    """
+    table = "product_type"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["name"]["type"], String)
+    assert isinstance(columns["level"]["type"], Integer)
+    assert isinstance(columns["parent_id"]["type"], Integer)
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    """
+    테이블의 Nullable, Non-Nullable 필드가 정상 설정되어 있는지 테스트
+    """
+    table = "product_type"
+    columns = db_inspector.get_columns(table)
+
+    expected_nullable = {
+        "id":        False,
+        "name":      False,
+        "level":     False,
+        "parent_id": True,
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(column_name), f"column {column_name} is not as null expected"
+
+
+def test_model_structure_column_constraints(db_inspector):
+    """
+    테이블의 Constraints 설정이 정상 설정되어 있는지 테스트
+    """
+    table = "product_type"
+    constraints = db_inspector.get_check_constraints(table)
+
+    assert any(constraint["name"] == "product_type_name_length_check" for constraint in constraints)
+
+
+def test_model_structure_column_lengths(db_inspector):
+    """
+    테이블의 길이 제한이 잘 적용되어 있는지 확인
+    """
+
+    table = "product_type"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert columns["name"]["type"].length == 100
+
+
+def test_model_structure_unique_constraints(db_inspector):
+    """
+    테이블의 Unique Constrints가 잘 적용되어 있는지 확인
+    """
+    table = "product_type"
+    constraints = db_inspector.get_unique_constraints(table)
+
+    assert any(constraint["name"] == "uq_product_type_name_level" for constraint in constraints)
