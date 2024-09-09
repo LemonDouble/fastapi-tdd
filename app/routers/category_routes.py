9d3bffe5e38dda34728c8435770dbc385b0ee201
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-09 15:44:34 +0900

    feat : 테스트에 맞춰 카테고리 생성 API 추가

---
+++ app/routers/category_routes.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.db_connection import get_db_session
+from app.models import Category
+from app.schema.category_schema import CategoryReturn, CategoryCreate
+
+router = APIRouter()
+
+@router.post("/", response_model=CategoryReturn, status_code=HTTPStatus.CREATED)
+def create_category(category_data : CategoryCreate, db : Session =  Depends(get_db_session)):
+    new_category = Category(**category_data.model_dump())
+    db.add(new_category)
+    db.commit()
+    db.refresh(new_category)
+    return new_category
