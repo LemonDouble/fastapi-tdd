0a505e8e87f013784f2ee8fe52848eadbd6736fe
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-09 17:58:45 +0900

    feat : 테스트에 맞춰 Category 중복 체크 로직 구현

--- app/routers/category_routes.py
+++ app/routers/category_routes.py
@@ -6,11 +6,15 @@ from sqlalchemy.orm import Session
 from app.db_connection import get_db_session
 from app.models import Category
 from app.schema.category_schema import CategoryReturn, CategoryCreate
+from app.utils.category_utils import check_existing_category
 
 router = APIRouter()
 
 @router.post("/", response_model=CategoryReturn, status_code=HTTPStatus.CREATED)
 def create_category(category_data : CategoryCreate, db : Session =  Depends(get_db_session)):
+
+    check_existing_category(db=db, category_data=category_data)
+
     new_category = Category(**category_data.model_dump())
     db.add(new_category)
     db.commit()
---
+++ app/utils/category_utils.py
@@ -0,0 +1,22 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from app.models import Category
+from app.schema.category_schema import CategoryCreate
+
+
+def check_existing_category(db: Session, category_data : CategoryCreate):
+    existing_category = (
+        db.query(Category).filter(
+            (Category.slug == category_data.slug) |
+            (Category.name == category_data.name) & (Category.level == category_data.level)
+        ).first()
+    )
+
+    if existing_category:
+        if existing_category.name == category_data.name and existing_category.level == category_data.level:
+            detailed_message = "Category with this name and level exists"
+        else:
+            detailed_message = "Category with this slug exists"
+
+        raise HTTPException(status_code=400, detail=detailed_message)
