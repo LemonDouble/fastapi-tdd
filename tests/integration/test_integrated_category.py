de67c3987ea2559087a84358ade3238470928e8b
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-12 12:01:11 +0900

    test : 카테고리 업데이트 API 테스트 추가

--- tests/integration/test_integrated_category.py
+++ tests/integration/test_integrated_category.py
@@ -60,3 +60,31 @@ def test_integrate_get_all_categories(client, db_session_integration):
 
     for returned_category, inserted_category in zip(returned_categories, categories):
         assert returned_category == inserted_category
+
+
+def test_integrate_update_category_successful(client, db_session_integration):
+    initial_category_data = get_random_category_dict()
+    new_category = Category(**initial_category_data)
+    db_session_integration.add(new_category)
+    db_session_integration.commit()
+
+    updated_category_data = {
+        "name": "Updated Name",
+        "slug": "updated-slug",
+        "is_active": False,
+        "level": 10,
+    }
+
+    response = client.put(f"api/category/{new_category.id}", json=updated_category_data)
+
+    assert response.status_code == 201
+
+    updated_category = (
+        db_session_integration.query(Category).filter_by(id=new_category.id).first()
+    )
+
+    assert updated_category is not None
+    assert updated_category.name == updated_category_data["name"]
+    assert updated_category.slug == updated_category_data["slug"]
+    assert updated_category.is_active == updated_category_data["is_active"]
+    assert updated_category.level == updated_category_data["level"]
