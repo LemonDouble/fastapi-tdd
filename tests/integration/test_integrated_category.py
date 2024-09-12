5be0c8e13b2855da28cd9fb8fd960d4d08c7dd0d
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-12 11:43:53 +0900

    test : 카테고리 전체 조회 통합 테스트 추가

--- tests/integration/test_integrated_category.py
+++ tests/integration/test_integrated_category.py
@@ -39,3 +39,24 @@ def test_integrate_create_new_category_duplicated(client, db_session_integration
     response = client.post("api/category/", json=category_data)
 
     assert response.status_code == 400
+
+
+def test_integrate_get_all_categories(client, db_session_integration):
+    categories = [get_random_category_dict() for _ in range(5)]
+
+    for category_data in categories:
+        new_category = Category(**category_data)
+        db_session_integration.add(new_category)
+        db_session_integration.commit()
+
+    response = client.get("/api/category/")
+
+    assert response.status_code == 200
+    assert response.json() is not None
+
+    returned_categories = response.json()
+    assert isinstance(returned_categories, list)
+    assert len(returned_categories) == len(categories)
+
+    for returned_category, inserted_category in zip(returned_categories, categories):
+        assert returned_category == inserted_category
