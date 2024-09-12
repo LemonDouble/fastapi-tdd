f60bf521dfb2010348c1799c8a184db0dedaf811
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2024-09-12 11:10:52 +0900

    test : 카테고리 생성 통합 테스트 추가

--- tests/integration/test_integrated_category.py
+++ tests/integration/test_integrated_category.py
@@ -2,9 +2,28 @@ from app.models import Category
 from tests.factories.models_factory import get_random_category_dict
 
 
-def test_test(client, db_session_integration):
+def test_integrate_create_new_category_successful(client, db_session_integration):
+    # Arrange : Prepare test data
     category_data = get_random_category_dict()
+    category_data.pop("id")
 
-    new_category = Category(**category_data)
-    db_session_integration.add(new_category)
-    db_session_integration.commit()
+    # Act
+    response = client.post("api/category/", json=category_data)
+
+    # Assert : Verify Response
+    assert response.status_code == 201
+
+    # Assert : Verify the Response and Database State
+    created_category = (
+        db_session_integration.query(Category)
+        .filter_by(id=response.json()["id"])
+        .first()
+    )
+
+    assert created_category is not None
+
+    # Assert : Verify response data matches database entry
+    assert response.json() == {
+        column.name: getattr(created_category, column.name)
+        for column in created_category.__table__.columns
+    }
